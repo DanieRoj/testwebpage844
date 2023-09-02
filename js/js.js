bb7b1a32d9e03f21a33922259980b2fe4cbf5a52
Danie Deow <127192609+DanieRoj@users.noreply.github.com>
2023-09-02 17:53:34 +0530

    2.when it load

--- js/js.js
+++ js/js.js
@@ -154,3 +154,8 @@ $(function () {
 
 
 // ---- form ---- //
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector("body").classList.add("loaded");
+    }, 10)
+});
